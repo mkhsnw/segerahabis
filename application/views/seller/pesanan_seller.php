99f675cf329cd149f97abd903f2ea1e2124d9b63
Lingga <linggaept@gmail.com>
2024-06-24 15:18:53 +0700

    seller lagi

--- application/views/seller/pesanan_seller.php
+++ application/views/seller/pesanan_seller.php
@@ -566,11 +566,22 @@
                                                                     hover:text-black focus:z-10 ">
                                             Batal
                                         </button>
-                                        <button type="submit" class="py-2 px-5 text-sm font-medium text-center text-white bg-red-600 
-                                                                    rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 
-                                                                    dark:hover:bg-red-600 dark:focus:ring-red-900">
-                                            Hapus
-                                        </button>
+                                        <button id="gagalButton" data-modal-target="gagal" data-modal-toggle="gagal" class="block text-sm font-medium text-white bg-red-800 border rounded-lg px-4 py-2 hover:bg-red-900" type="button">Hapus</button>
+
+                                        <div id="gagal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                            <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+                                                <!-- Modal content -->
+                                                <div class="relative px-2 py-6 text-center bg-white rounded-lg shadow">
+                                                    <div class="w-12 h-12 rounded-full bg-red-200 p-2 flex items-center justify-center mx-auto mb-3.5">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10 text-red-700">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                                                        </svg>
+                                                        <span class="sr-only">Gagal</span>
+                                                    </div>
+                                                    <p class=" text-lg font-bold text-gray-900">Gagal Menolak Pesanan</p>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
